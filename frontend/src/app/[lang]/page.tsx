1afbb5cd4af2473f144811206e92ad87deb9dc93
Noah Emmenegger <noahemmenegger.business@gmail.com>
2023-04-17 20:28:48 +0200

    Add locale request

--- frontend/src/app/[lang]/page.tsx
+++ frontend/src/app/[lang]/page.tsx
@@ -1,18 +1,18 @@
 import { fetchAPI } from './utils/fetch-api';
 import { sectionRenderer } from './utils/section-renderer';
 
-async function getPageBySlug(slug: string) {
+async function getPageBySlug(slug: string, lang: string) {
     const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
     const path = `/pages`;
-    const urlParamsObject = { filters: { slug } };
+    const urlParamsObject = { filters: { slug }, locale: lang };
     const options = { headers: { Authorization: `Bearer ${token}` } };
     const response = await fetchAPI(path, urlParamsObject, options);
     return response;
 }
 
-export default async function RootRoute() {
-    const page = await getPageBySlug('home');
+export default async function RootRoute({ params }: { params: { lang: string } }) {
+    const page = await getPageBySlug('home', params.lang);
     const contentSections = page.data[0].attributes.contentSections;
 
     return contentSections.map((section: any, index: number) => sectionRenderer(section, index));
