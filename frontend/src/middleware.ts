ac98077bab28f4ebad72759f50fc7143cf916eee
Noah Emmenegger <noahemmenegger.business@gmail.com>
2023-04-17 11:00:33 +0200

    update middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -23,14 +23,14 @@ export function middleware(request: NextRequest) {
 
     // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
     // // If you have one
-    // if (
-    //   [
-    //     '/manifest.json',
-    //     '/favicon.ico',
-    //     // Your other files in `public`
-    //   ].includes(pathname)
-    // )
-    //   return
+    if (
+        [
+            '/manifest.json',
+            '/favicon.ico',
+            // Your other files in `public`
+        ].includes(pathname)
+    )
+        return;
 
     // Check if there is any supported locale in the pathname
     const pathnameIsMissingLocale = i18n.locales.every(
@@ -49,5 +49,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
     // Matcher ignoring `/_next/` and `/api/`
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+    matcher: ['/((?!_next).*)'],
 };
