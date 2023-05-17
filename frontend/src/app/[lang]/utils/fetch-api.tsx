b0f6a79a7a388aab5ba6fbc8d41628c32790f488
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2023-05-17 12:50:05 -0500

    fixing the fetch-api.tsx not fetching data

--- frontend/src/app/[lang]/utils/fetch-api.tsx
+++ frontend/src/app/[lang]/utils/fetch-api.tsx
@@ -1,7 +1,11 @@
 import qs from "qs";
 import { getStrapiURL } from "./api-helpers";
 
-export async function fetchAPI(path: string, urlParamsObject = {}, options = {}) {
+export async function fetchAPI(
+  path: string,
+  urlParamsObject = {},
+  options = {}
+) {
   try {
     // Merge default and user options
     const mergedOptions = {
@@ -14,7 +18,7 @@ export async function fetchAPI(path: string, urlParamsObject = {}, options = {})
     // Build request URL
     const queryString = qs.stringify(urlParamsObject);
     const requestUrl = `${getStrapiURL(
-      `/api${path}${queryString ? queryString : ""}`
+      `/api${path}${queryString ? `?${queryString}` : ""}`
     )}`;
 
     // Trigger API call
@@ -22,19 +26,13 @@ export async function fetchAPI(path: string, urlParamsObject = {}, options = {})
 
     // Handle response
     if (!response.ok) {
+      console.error(response.statusText);
       throw new Error(`An error occurred please try again`);
     }
-
     const data = await response.json();
     return data;
-
   } catch (error) {
-    console.log("From try catch", error)
-    if (error instanceof Error) {
-      console.error(`An error occurred while fetching the API please check your server: ${error.message}`);
-    } else {
-      console.error(`An error occurred while fetching the API please check your server.`);
-    }
-    throw new Error("An error occurred while fetching the API please check your server.");
+    console.error(error);
+    throw new Error(`Please check if your server is running and you set all the required tokens.`);
   }
 }
