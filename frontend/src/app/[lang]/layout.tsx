97373ce13b2aea53c4caeabdbab562079af5ee09
Anas Gamal <agamal@stetson.edu>
2023-07-04 17:57:00 +0300

    Refactor RootLayout to support localization

--- frontend/src/app/[lang]/layout.tsx
+++ frontend/src/app/[lang]/layout.tsx
@@ -10,7 +10,7 @@ import Navbar from "./components/Navbar";
 import {FALLBACK_SEO} from "@/app/[lang]/utils/constants";
 
 
-async function getGlobal(): Promise<any> {
+async function getGlobal(lang: string): Promise<any> {
   const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
   if (!token) throw new Error("The Strapi API Token environment variable is not set.");
@@ -31,12 +31,13 @@ async function getGlobal(): Promise<any> {
       "footer.socialLinks",
       "footer.categories",
     ],
+    locale: lang,
   };
   return await fetchAPI(path, urlParamsObject, options);
 }
 
-export async function generateMetadata(): Promise<Metadata> {
-  const meta = await getGlobal();
+export async function generateMetadata({ params } : { params: {lang: string}}): Promise<Metadata> {
+  const meta = await getGlobal(params.lang);
 
   if (!meta.data) return FALLBACK_SEO;
 
@@ -59,7 +60,7 @@ export default async function RootLayout({
   children: React.ReactNode;
   params: { lang: string };
 }) {
-  const global = await getGlobal();
+  const global = await getGlobal(params.lang);
   // TODO: CREATE A CUSTOM ERROR PAGE
   if (!global.data) return null;
   
