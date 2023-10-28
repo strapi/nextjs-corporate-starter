61f6c379d07c5553128e804b28ff7a23ef177bc4
Loi Tran <datloiboi@gmail.com>
2023-10-28 15:17:27 -0400

    Add prompt in console for missing token (#96)
    @PrimeTimeTran Thank you so much.  You are awesome.

--- frontend/src/app/[lang]/page.tsx
+++ frontend/src/app/[lang]/page.tsx
@@ -4,9 +4,20 @@ import {getPageBySlug} from "@/app/[lang]/utils/get-page-by-slug";
 
 
 export default async function RootRoute({params}: { params: { lang: string } }) {
-    const page = await getPageBySlug('home', params.lang);
-    if (page.data.length == 0 && params.lang !== "en") return <LangRedirect/>
-    if (page.data.length === 0) return null;
-    const contentSections = page.data[0].attributes.contentSections;
-    return contentSections.map((section: any, index: number) => sectionRenderer(section, index));
+    try {
+      const page = await getPageBySlug('home', params.lang)
+      if (page.error && page.error.status == 401)
+        throw new Error(
+          'Missing or invalid credentials. Have you created an access token using the Strapi admin panel? http://localhost:1337/admin/'
+        )
+
+      if (page.data.length == 0 && params.lang !== 'en') return <LangRedirect />
+      if (page.data.length === 0) return null
+      const contentSections = page.data[0].attributes.contentSections
+      return contentSections.map((section: any, index: number) =>
+        sectionRenderer(section, index)
+      )
+    } catch (error: any) {
+      window.alert('Missing or invalid credentials')
+    }
 }
