2323497c3df9b721b9198156e1f8bda05225c12c
obsfeil <obsfeil@outlook.com>
2024-05-05 22:26:22 +0200

    k

--- backen/src/api/product-feature/routes/product-feature.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * product-feature router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::product-feature.product-feature');
--- backen/src/api/product-feature/services/product-feature.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * product-feature service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::product-feature.product-feature');
