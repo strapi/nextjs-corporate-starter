f093c74e4fea1f9db7a00d948e610f9403b50ada
Jesse Clark <gearsandcode@users.noreply.github.com>
2024-09-12 16:48:03 -0700

    fix: add safe check for seo.attributes and defaultLocale fallback (#142)

--- frontend/src/app/[lang]/[...slug]/page.tsx
+++ frontend/src/app/[lang]/[...slug]/page.tsx
@@ -15,7 +15,7 @@ type Props = {
 export async function generateMetadata({params}: Props): Promise<Metadata> {
     const page = await getPageBySlug(params.slug, params.lang);
 
-    if (!page.data[0].attributes?.seo) return FALLBACK_SEO;
+    if (!page.data[0]?.attributes?.seo) return FALLBACK_SEO;
     const metadata = page.data[0].attributes.seo
 
     return {
